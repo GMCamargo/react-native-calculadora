01af673ac933bd37442b857447436a77be8fdfc1
GMCamargo <bielmarcom@gmail.com>
2020-07-29 21:48:31 -0300

    Continuação do app

--- src/App.js
+++ src/App.js
@@ -9,50 +9,53 @@ import Display from './components/Display';
 
 export default () => {
 
-  const [state,setState] = useState('0')
+  const [state, setState] = useState('0')
 
-  clearMemory = () =>{
+  clearMemory = () => {
     setState('0')
   }
   addDigit = (n) => {
-    if(state != '0') setState(state + n);
+    if (state != '0') { 
+      if(n == ',' && state.includes(',')) setState(state)
+      else setState(state + n); 
+    }
     else setState(n)
   }
 
-  setOperation = op =>{
-    
+  setOperation = op => {
+
   }
-    return (
-      <SafeAreaView style={styles.container}>
-        <Display value={state} />
-        <View style={styles.buttons}>
-          {/* First Row*/}
-          <Button label='CE' onClick = {this.clearMemory} />
-          <Button label='C' />
-          <Button label='<=' />
-          <Button label='/' op onClick={() => this.setOperation('/')}/>
-          {/* Second Row*/}
-          <Button label='7' onClick={addDigit} />
-          <Button label='8' onClick={() => this.addDigit(8)}/>
-          <Button label='9' onClick={() => this.addDigit(9)}/>
-          <Button label='*' op onClick={() => this.setOperation('*')}/>
-          {/* Thrid Row*/}
-          <Button label='4' onClick={() => this.addDigit(4)}/>
-          <Button label='5' onClick={() => this.addDigit(5)}/>
-          <Button label='6' onClick={() => this.addDigit(6)}/>
-          <Button label='-' op onClick={() => this.setOperation('-')}/>
-          {/* Fourth Row*/}
-          <Button label='1' onClick={() => this.addDigit(1)}/>
-          <Button label='2' onClick={() => this.addDigit(2)}/>
-          <Button label='3' onClick={() => this.addDigit(3)}/>
-          <Button label='+' op onClick={() => this.setOperation('+')}/>
-          {/* Fifth Row*/}
-          <Button label='0' double onClick={() => this.addDigit(0)}/>
-          <Button label=',' onClick={() => this.addDigit(',')}/>
-          <Button label='=' op onClick={() => this.setOperation('=')}/>
-        </View>
-      </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={styles.container}>
+      <Display value={state} />
+      <View style={styles.buttons}>
+        {/* First Row*/}
+        <Button label='CE' onClick={this.clearMemory} />
+        <Button label='C' />
+        <Button label='<=' />
+        <Button label='/' op onClick={setOperation} />
+        {/* Second Row*/}
+        <Button label='7' onClick={addDigit} />
+        <Button label='8' onClick={addDigit} />
+        <Button label='9' onClick={addDigit} />
+        <Button label='*' op onClick={setOperation} />
+        {/* Thrid Row*/}
+        <Button label='4' onClick={addDigit} />
+        <Button label='5' onClick={addDigit} />
+        <Button label='6' onClick={addDigit} />
+        <Button label='-' op onClick={setOperation} />
+        {/* Fourth Row*/}
+        <Button label='1' onClick={addDigit} />
+        <Button label='2' onClick={addDigit} />
+        <Button label='3' onClick={addDigit} />
+        <Button label='+' op onClick={setOperation} />
+        {/* Fifth Row*/}
+        <Button label='0' double onClick={addDigit} />
+        <Button label=',' onClick={addDigit} />
+        <Button label='=' op onClick={setOperation} />
+      </View>
+    </SafeAreaView>
+  )
 
 };
 
